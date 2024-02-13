c97498d98a99638f77bfa5e8c8c37461256d350c
godsfavours <godsfavourcs@gmail.com>
2024-02-13 13:21:42 -0800

    create login & signup components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,18 @@
-import React from "react";
-import {BrowserRouter, Route,Routes} from 'react-router-dom';
-import { Link } from 'react-router';
-import {Journal} from "./Journal";
-import {Home} from "./Home";
-import {variables} from './Variables.js';
-import NavBar from './NavBar';
-import routes from './routes';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './components/Login/Login'
+import Signup from './components/Signup/Signup'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App container">
-      <h3 className="d-flex justify-content-center m-3">
-        Welcome to JournalJam
-      </h3>
-
-      <BrowserRouter>
-      <NavBar />
-      <div className="container mt-4">
-        <Routes>
-          {routes.map((route, index) => (
-            <Route key={index} path={route.path} element={route.component} />
-          ))}
-        </Routes>
-      </div>
-    </BrowserRouter>
-  
-
-      <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/journal_entries" element={<Journal />} />
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
       </Routes>
     </BrowserRouter>
-
-    </div>
   )
 }
 
-export default App;
+export default App

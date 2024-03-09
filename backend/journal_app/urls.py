4f02ae71a3be9b4564cd7575f98164aa853c6eeb
Mohamed Owda <mohamedowda@DNa8093bb.SUNet>
2024-03-09 13:59:05 -0800

    fix: delete unused apis

--- backend/journal_app/urls.py
+++ backend/journal_app/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
     path('api/user/', views.CreateUserAPIView.as_view(), name='api-create-user'),
     path('api/user/<int:user_id>/', views.GetUserAPIView.as_view(), name='api-get-user'),
     path('api/current_user/', views.CurrentUserAPIView.as_view(), name='api-current-user'),
@@ -12,7 +11,4 @@ urlpatterns = [
     path('entries/', views.CreateJournalEntryAPIView.as_view(), name='create_journal_entry'),
     path('api/entries/<int:entry_id>/', views.JournalEntryDetailAPIView.as_view(), name='journal_entry_details'),
     path('api/entries/<int:entry_id>/content/', views.JournalEntryContentDetailAPIView.as_view(), name='journal_entry_content_details'),
-    path('login/', views.user_login, name='login'),
-    path('signup/', views.user_signup, name='signup'),
-    path('logout/', views.user_logout, name='logout'),
 ]
